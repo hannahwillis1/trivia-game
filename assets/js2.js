ac9d19932c331478baa1a64b47209480354978e9
Hannah Willis <han.e.willis@gmail.com>
2018-11-21 07:35:23 -0600

    trying again

---
+++ assets/js2.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  $("#start").click(function() {
+    var number = 60;
+    alert("Get ready!");
+    $("#start").on("click", start);
+    $("#submit").on("click", finish);
+    $("#restart").on("click", restart);
+
+    function start() {
+      counter = setInterval(timer, 2000);
+      showMe(".question");
+      showMe(".answers");
+      showMe("#submit");
+      hideMe("#start");
+      hideMe(".how-to-play");
+      hideMe("#restart");
+      hideMe("#results");
+    }
+    function timer() {
+      number--;
+      $("#show-number").html("<h2>" + number + "</h2>");
+      if (number === 0) {
+        alert("Sorry - you're out of time!!");
+        stop();
+      }
+    }
+    function stop() {
+      clearInterval(counter);
+      $("#results").show();
+      $("#restart").show();
+      $(".question").hide();
+      $(".answers").hide();
+      $("#submit").hide();
+    }
+    function finish() {
+      number = 1;
+      clearInterval(counter);
+      timer();
+    }
+
+    function restart() {
+      number = 60;
+      start();
+    }
+
+    function hideMe(e) {
+      $(e).hide();
+    }
+    function showMe(e) {
+      $(e).show();
+    }
+
+    start();
+  });
+});
